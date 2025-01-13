4e9577480a98f3c406381306260c37a42cd50221
Your Name <you@example.com>
2025-01-14 00:53:24 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+require("config.set")
+
+function Transparent(color)
+    color = color or "tokyonight"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+Transparent()
